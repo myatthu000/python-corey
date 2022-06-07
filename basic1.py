a37fac2c9d5ea81bfc18e557502a894136bc1fd1
myatthu000 <september.2019thu@gmail.com>
2022-06-07 23:54:33 +0630

    lesson 1 branch

--- basic1.py
+++ basic1.py
@@ -1,3 +1,26 @@
-print("hello world")
+# String dir help f-string
 
-print(dir())
+# print("hello world")
+
+# message = "this's is test"
+# print(message)
+# print(message[0:])
+
+greeting = "Hello"
+name = "John"
+
+# message = greeting + ', ' + name + ". Welcome!"
+# message = '{} {}. Welcome!'.format(greeting,name)
+# message = f'{greeting} {name}. Welcome!'
+
+
+# print(message)
+
+# print(help(str.title))
+# print(dir(str))
+directory = dir(str)
+
+# j = 0
+# for i in directory:
+#     j += 1
+#     print(f'{j} ---> {i}',type(i))
